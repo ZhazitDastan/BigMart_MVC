2c17145c9d0a9baccf5fa06de93a607703aa9a64
Dastan <zhazit.dastan04@gmail.com>
2024-10-01 18:56:53 +0500

    Add project files.

---
+++ BigMartWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BigMartWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

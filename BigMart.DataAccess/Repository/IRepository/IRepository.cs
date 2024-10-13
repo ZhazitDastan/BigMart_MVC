3aa233629b75b023464eccfb7650ca5dc06dbc2e
ZhazitDastan <zhazit.dastan04@gmail.com>
2024-10-13 22:11:22 +0500

    The product updated

--- BigMart.DataAccess/Repository/IRepository/IRepository.cs
+++ BigMart.DataAccess/Repository/IRepository/IRepository.cs
@@ -10,8 +10,8 @@ namespace BigMart.DataAccess.Repository.IRepository
     public interface IRepository<T> where T : class
     {
         //T - Category
-        IEnumerable<T> GetAll();
-        T Get(Expression<Func<T, bool>> filters);
+        IEnumerable<T> GetAll(string? includeProperties = null);
+        T Get(Expression<Func<T, bool>> filters, string? includeProperties = null);
         void Add(T entity);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entity);
